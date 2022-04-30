4fd63de7585df40b4177e4f0fcc14b94418b7882
waki68 <wakiwiki@live.com>
2022-04-30 21:38:05 +0430

    number 2 and 3

--- RomanNumerals/RomanNumeralComplexTest.cs
+++ RomanNumerals/RomanNumeralComplexTest.cs
@@ -7,12 +7,24 @@ namespace RomanNumerals;
 public class RomanNumeralComplexTest
 {
     [Theory]
-    [InlineData("II",2)]
+    [InlineData("II", 2)]
+    [InlineData("III", 3)]
     public void return_corresponding_number_for_two_and_three(string romanNumeral, int expectedNumber)
     {
         var characters = romanNumeral.ToCharArray();
-        var _two = $"{characters[1]}".FromBaseRomanNumeralSymbols();
-        _two += $"{characters[0]}".FromBaseRomanNumeralSymbols();
-        _two.Should().Be(2);
+        int number = 0;
+        if (expectedNumber == 2)
+        {
+            number += $"{characters[1]}".FromBaseRomanNumeralSymbols();
+            number += $"{characters[0]}".FromBaseRomanNumeralSymbols();
+        }
+
+        if (expectedNumber == 3)
+        {
+            number += $"{characters[2]}".FromBaseRomanNumeralSymbols();
+            number += $"{characters[1]}".FromBaseRomanNumeralSymbols();
+            number += $"{characters[0]}".FromBaseRomanNumeralSymbols();
+        }
+        number.Should().Be(expectedNumber);
     }
 }
